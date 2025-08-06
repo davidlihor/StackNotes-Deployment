675e52db36c947a0c341493857649f22500fcdac
David <davidlihor.dev@gmail.com>
2025-08-06 16:03:20 +0300

    Setup Argo CD with TLS, ingress, and initial app deployment

--- start.sh
+++ start.sh
@@ -7,32 +7,18 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 current_dir=$(pwd)
-# minikube cp ./keycloak-config/data/import/keycloak.json /data/import/keycloak.json
-
-# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-# helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
-# helm repo update
-
-# helm dependency update $current_dir/infra/helm
-# helm install nginx ingress-nginx/ingress-nginx --namespace ingress-nginx --create-namespace
-# helm install prometheus prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace
-# helm install carsties $current_dir/infra/helm --namespace default --create-namespace
-
 
 cd "$current_dir"/devcerts
-mkcert -key-file stacknotes-local-tls.key -cert-file stacknotes-local-tls.crt app.stacknotes.local api.stacknotes.local
+mkcert -key-file stacknotes-tls.key -cert-file stacknotes-tls.crt app.stacknotes.local api.stacknotes.local
+mkcert -key-file argocd-ui-tls.key -cert-file argocd-ui-tls.crt argocd.stacknotes.local
+mkcert -key-file argocd-grpc-tls.key -cert-file argocd-grpc-tls.crt grpc.argocd.stacknotes.local
 chmod -R +rwx .
 
-# cd "$current_dir"/infra/devcerts
-# mkcert -key-file carsties-app-tls.key -cert-file carsties-app-tls.crt app.carsties.local api.carsties.local id.carsties.local
-# mkcert -key-file monitoring-tls.key -cert-file monitoring-tls.crt prometheus.monitoring.local grafana.monitoring.local
-
-
 cd "$current_dir"
-CERTS_DIR="$current_dir"/devcerts #/infra/devcerts
+CERTS_DIR="$current_dir"/devcerts
 
 if [ ! -d "$CERTS_DIR" ]; then
-    echo "Cert folder not found";
+    echo "Cert folder not found"
     exit 1
 fi
 
@@ -49,13 +35,13 @@ for cert in "$CERTS_DIR"/*.crt; do
         echo "WARNING! Key for $cert not found"
         continue;
     fi
-
-    kubectl delete secret "$secret_name" || true
     
-    if [[ "$key" != *"monitoring"* ]]; then
-        kubectl create secret tls "$secret_name" --key="$key" --cert="$cert"
-    else
+    if [[ "$key" == *"monitoring"* ]]; then
         kubectl create secret tls "$secret_name" --key="$key" --cert="$cert" --namespace=monitoring
+    elif [[ "$key" == *"argocd"* ]]; then
+        kubectl create secret tls "$secret_name" --key="$key" --cert="$cert" --namespace=argocd
+    else
+        kubectl create secret tls "$secret_name" --key="$key" --cert="$cert"
     fi
     
     echo "Secret $secret_name created from $key and $cert"
@@ -63,7 +49,7 @@ done;
 
 
 IP="127.0.0.1"
-DOMAIN="app.stacknotes.local api.stacknotes.local"
+DOMAIN="grpc.argocd.stacknotes.local argocd.stacknotes.local"
 LINE="$IP $DOMAIN"
 
 if grep -Fxq "$LINE" /etc/hosts; then
